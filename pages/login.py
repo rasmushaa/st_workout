fb5743d550985686bf6bcbdba1a6e77602823c01
rasmushaa <rasmus.haapaniemi@outlook.com>
2024-10-25 18:23:59 +0300

    Add requirements

--- pages/login.py
+++ pages/login.py
@@ -25,7 +25,7 @@ if st.button('Login', icon=":material/login:"):
     if st_wrapper_password_check(username, password_hash):
         st.success('Login successful')
         st.session_state['user'] = st_wrapper_init_user(username, password_hash)
-        st.switch_page('pages/exercise.py')
+        st.switch_page('pages/main_window.py')
     
     else:
         st.error('Password and Username do not match')
