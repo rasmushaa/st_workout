af4564be32dd0af968be9092c807e76e065cd22b
rasmushaa <rasmus.haapaniemi@outlook.com>
2024-10-26 21:25:38 +0300

    Debug yaml

--- run_dev_docker.sh
+++ run_dev_docker.sh
@@ -7,4 +7,4 @@ echo "----> Stopping and removing any running containers from the previous build
 docker-compose -f $COMPOSE_FILE down
 
 echo "----> Building and running the new Docker image with Docker Compose"
-docker-compose -f $COMPOSE_FILE up --build -d
+docker-compose --env-file .env -f $COMPOSE_FILE up --build -d
