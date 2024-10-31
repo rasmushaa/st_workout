125c22d51b5fa8eb2922da19cfc56081be305d04
rasmushaa <rasmus.haapaniemi@outlook.com>
2024-10-31 18:38:57 +0200

    Redefine service structure

--- backend/bigquery_api.py
+++ backend/bigquery_api.py
@@ -8,7 +8,7 @@ from google.oauth2 import service_account
 
 class BigQueryAPI():
     def __init__(self):
-        self.__project_id = os.getenv("GCP_PROJECT_ID")
+        self.__project_id = 'rasmus-prod'
         self._dataset = f'st_workout_{os.getenv("STREAMLIT_ENV")}'
         self.__location = 'europe-north1'
 
